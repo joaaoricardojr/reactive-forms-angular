2473d4fe4c8102ee3c6b329d24a5e2f85aa7f97a
joaaoricardojr <joaoricardo.rsj@gmail.com>
2022-09-13 16:16:35 -0300

    initial commit

---
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent implements OnInit {
+
+  public cadastroForm: FormGroup = this.formBuilder.group({
+    firstName: ['']
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
